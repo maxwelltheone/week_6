8dec1b667f9d5b77e6bda45b009f4e8e6ba5f0e0
Luevanos <pluevanos@cps.k12.il.us>
2024-10-07 09:34:26 -0500

    Start of challenge

---
+++ day3_challenge.py
@@ -0,0 +1,18 @@
+##################### DAY 3 CHALLENGE########################
+
+
+text = input("Enter and/or type in a text: ").lower()
+
+letter1 = input("Enter a random letter: ")
+letter2 = input("Enter a random letter: ")
+letter3 = input("Enter a random letter: ")
+
+
+
+
+
+
+
+
+
+
